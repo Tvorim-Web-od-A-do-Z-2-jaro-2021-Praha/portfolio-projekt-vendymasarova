c2cccca5cddc4e3bf8cbd8e99997345209b4ab48
vendymasarova <vendymasarova@seznam.cz>
2021-05-25 09:50:00 +0200

    last commit before push

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const navigation = function () {
-  const burger = document.querySelector(".burger");
-  const menu = document.querySelector(".nav__links");
+// const navigation = function () {
+//   const burger = document.querySelector(".burger");
+//   const menu = document.querySelector(".nav__links");
 
-  burger.addEventListener("click", function () {
-    menu.classList.toggle("nav__active");
-    console.log("hello");
-  });
-};
-navigation();
+//   burger.addEventListener("click", function () {
+//     menu.classList.toggle("nav__active");
+//     console.log("hello");
+//   });
+// };
+// navigation();
+// //////////////
+// // //title animation svg
+// // const logo = document.querySelectorAll("#logo path");
+// // for (let i = 0; i < logo.length; i++) {
+// //   console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+// // }
